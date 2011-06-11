99a2e91464fc0d407ee9f2fefeb2de390b737355
unknown <Master@.(none)>
2011-06-11 16:14:39 +0200

    Simplified by removing dependencies

--- app.js
+++ app.js
@@ -5,28 +5,16 @@
 
    Invoke from bash script like:
 
-     curl --form source="<${1}" ${JSLINT_URL}
+     curl --form source="<@{1}" ${JSLINT_URL}
 
-   If you use source="@${1}" instead, curl does it like a file upload.
-   That includes a filename, which is nice, but has express make a
-   temp file for the upload.
+   Adapted from https://github.com/keturn/lintnode
+     by removing node module dependencies
 */
 
-/*global process, require */
-var express = require("express");
-var JSLINT = require('./fulljslint');
-var fs = require('fs');
-
-var app = express.createServer();
-
-app.configure(function () {
-    app.use(require('connect-form')({keepExtensions: true}));
-    app.use(express.errorHandler(
-        { dumpExceptions: true, showStack: true }));
-    app.use(express.bodyDecoder());
-});
-
-var jslint_port = 3003;
+/*global require, process */
+var http = require('http');
+var fulljslint = require('./fulljslint');
+var JSLINT = fulljslint.JSLINT;
 
 /* copied from jslint's rhino.js */
 var jslint_options = {
@@ -40,8 +28,10 @@ var jslint_options = {
     regexp: true,
     rhino: true,
     undef: true,
-    white: true
+    white: false
 };
+
+var jslintPort = 1337;
 
 var outputErrors = function (errors) {
     var e, i, output = [];
@@ -56,85 +46,55 @@ var outputErrors = function (errors) {
         if (e) {
             write('Lint at line ' + e.line + ' character ' +
                         e.character + ': ' + e.reason);
-            write((e.evidence || '').
-                        replace(/^\s*(\S*(\s+\S+)*)\s*$/, "$1"));
+            write((e.evidence || '').replace(/^\s*(\S*(\s+\S+)*)\s*$/, "$1"));
             write('');
         }
     }
     return output.join('');
 };
 
-app.get('/', function (req, res) {
-    res.render('upload.haml');
-});
 
-app.post('/jslint', function (request, res) {
-    var filename;
-    if (! request.form) {
-        throw new TypeError("form data required");
+var doLint = function (sourcedata) {
+    var passed, results;
+    passed = JSLINT(sourcedata, jslint_options);
+    if (passed) {
+        results = "jslint: No problems found\n";
+    } else {
+        results = outputErrors(JSLINT.errors);
     }
-    return request.form.complete(function (err, fields, files) {
-        var headers = {'Content-Type': 'text/plain'};
-
-        function doLint(sourcedata) {
-            var passed, results;
-            passed = JSLINT.JSLINT(sourcedata, jslint_options);
-            if (passed) {
-                // debug("no errors\n");
-                results = "jslint: No problems found in " + filename + "\n";
-            } else {
-                results = outputErrors(JSLINT.JSLINT.errors);
-                // debug("results are" + results);
-            }
-            return results;
-        }
-
-        if (files.source) {
-            // FIXME: It's pretty silly that we have express write the upload to
-            // a tempfile only to read the entire thing back into memory
-            // again.
-            filename = files.source.filename;
-            fs.readFile(files.source.path, 'utf8',
-                function (err, sourcedata) {
-                    var results;
-                    results = doLint(sourcedata);
-                    res.send(results, headers, 200);
-                    fs.unlink(files.source.path);
-                });
-        } else {
-            filename = fields.filename;
-            res.send(doLint(fields.source), headers, 200);
-        }
-    });
-});
-
-
-/* This action always return some JSLint problems. */
-var exampleFunc = function (req, res) {
-    JSLINT.JSLINT("a = function(){ return 7 + x }()",
-        jslint_options);
-    res.send(outputErrors(JSLINT.JSLINT.errors),
-        {'Content-Type': 'text/plain'});
+    return results;
 };
 
-app.get('/example/errors', exampleFunc);
-app.post('/example/errors', exampleFunc);
+var jslint = function (req, res){
+    var data = '';
+    req.addListener("data", function (chunk) {
+	data += chunk;
+    });
+    req.addListener("end", function() {
+        data = data.substr(data.indexOf("\r\n\r\n") + 4);
+	//Remove multi-part file upload data and headers
+	var filtered = '';	
+	data.split("\r\n").forEach(function(line){
+	    if(!line.match(/^-----/)&&!line.match(/^Content-/)){
+		filtered+=line+"\n";
+	    }
+	});
+	var lintData = doLint(filtered);
+	res.end(lintData);
+    });
+};
 
-
-/* This action always returns JSLint's a-okay message. */
-app.post('/example/ok', function (req, res) {
-    res.send("jslint: No problems found in example.js\n",
-        {'Content-Type': 'text/plain'});
-});
-
-
-function parseCommandLine() {
+var parseCommandLine = function () {
     var port_index = process.ARGV.indexOf('--port');
     if (port_index > -1) {
-        jslint_port = process.ARGV[port_index + 1];
+        jslintPort = process.ARGV[port_index + 1];
     }
-}
+};
 
 parseCommandLine();
 
-app.listen(jslint_port);
+var server = http.createServer(function (req, res) {
+    jslint(req, res);
+});
+
+server.listen(jslintPort, "127.0.0.1");
